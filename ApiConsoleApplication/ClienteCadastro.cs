71db893b62e6aadf936d59dd79a5b93c5da89013
Ssalvador221 <j8731615@gmail.com>
2023-07-25 22:35:26 -0300

    Finish Code

--- ApiConsoleApplication/ClienteCadastro.cs
+++ ApiConsoleApplication/ClienteCadastro.cs
@@ -1,3 +1,6 @@
+using ApiConsoleApplication.ExceptionSignInError;
+using SDKBrasilAPI;
+
 namespace ApiConsoleApplication;
 
 public class ClienteCadastro
@@ -6,7 +9,7 @@ public class ClienteCadastro
     public string? UltimoNomeCliente { get; set; }
     public int  Idade { get; set; }
     public string? Cep { get; set; }
-
+    
     public ClienteCadastro()
     {
         
@@ -20,5 +23,28 @@ public class ClienteCadastro
         Cep = cep;
     }
     
-    
+
+    public override string ToString()
+    {
+        return "Cadastro feito com Sucesso!\n" +
+               $"Bem-Vindo: {NomeCliente} {UltimoNomeCliente}\nSua idade: {Idade}\n";
+    }
+
+    public void IfNameIsNull() 
+    {
+        if (NomeCliente != null)
+        {
+            throw new SingInError("Opss... Firstname field is empty, please set a valid name!");
+        }        
+        if (UltimoNomeCliente != null)
+        {
+            throw new SingInError("Opss... Lastname field is empty, please set a valid name!");
+        }        
+        if (Idade != null || Idade <= 18)
+        {
+            throw new SingInError("Opss... Age field is empty or set a age greater than 18!");
+        }
+    }
+
+   
 }
--- ApiConsoleApplication/Program.cs
+++ ApiConsoleApplication/Program.cs
@@ -1,43 +1,87 @@
-﻿using System.Globalization;
-using ApiConsoleApplication;
-using Microsoft.VisualBasic;
+﻿using ApiConsoleApplication;
+using ApiConsoleApplication.ExceptionSignInError;
+using MySqlConnector;
 using SDKBrasilAPI;
 
-ClienteCadastro cliente = new ClienteCadastro();
 
 Console.Write("Seu primeiro Nome: ");
-cliente.NomeCliente = Console.ReadLine();
+string? firstName = Console.ReadLine();
 Console.Write("Seu ultimo Nome: ");
-cliente.UltimoNomeCliente = Console.ReadLine();
+string? lastName = Console.ReadLine();
 Console.Write("Sua Idade: ");
-cliente.Idade = Int32.Parse(Console.ReadLine());
+int idade = Int32.Parse(Console.ReadLine());
 Console.Write("Seu Cep: ");
-cliente.Cep = Console.ReadLine();
+string? cep = Console.ReadLine();
 Console.WriteLine();
 
-using (var brasilApi = new BrasilAPI())
+ClienteCadastro cliente = new ClienteCadastro(firstName, lastName, idade, cep);
+
+
+//Lista utilizada para receber os dados do cliente
+List<string> listaDadosApi = new List<string>();
+
+try
 {
-    try
+    //Verificação do CEP para que a API puxe os dados referente ao cep inserido
+    var verificarDados = cliente;
+    Console.WriteLine();
+    Console.WriteLine(verificarDados.ToString());
+    using (BrasilAPI api = new BrasilAPI())
     {
-        var cepCidade = await brasilApi.CEP(cliente.Cep);
-        var cepBairro = await brasilApi.CEP(cliente.Cep);
-        var cepRua = await brasilApi.CEP(cliente.Cep);
+        var nomeCidade = await api.CEP(cliente.Cep);
+        var nomeBairro = await api.CEP(cliente.Cep);
+        var nomeRua = await api.CEP(cliente.Cep);
+
         
-        foreach (var valorCep in cepCidade.City.Split((char)StringSplitOptions.RemoveEmptyEntries))
+        //Tratamento de dados da lista
+        foreach (var valorCep in nomeCidade.City.Split((char)StringSplitOptions.RemoveEmptyEntries))  
         {
-            Console.WriteLine($"Obrigado pelo cadastro Sr(a).{cliente.NomeCliente} {cliente.UltimoNomeCliente}");
-            foreach (var valorBairro in cepBairro.Neighborhood.Split((char)StringSplitOptions.RemoveEmptyEntries))
+            foreach (var valorBairro in nomeBairro.Neighborhood.Split((char)StringSplitOptions.RemoveEmptyEntries))
             {
-                foreach (var valorRua in cepRua.Street.Split((char)StringSplitOptions.RemoveEmptyEntries))
+                foreach (var valorRua in nomeRua.Street.Split((char)StringSplitOptions.RemoveEmptyEntries))
                 {
-                    Console.WriteLine($"Reside na Cidade: {valorCep}\nBairro: {valorBairro}\nRua: {valorRua}");
+                    listaDadosApi.Add(valorCep);
+                    listaDadosApi.Add(valorBairro);
+                    listaDadosApi.Add(valorRua);
+                    foreach (var lista in listaDadosApi)
+                    {
+                        Console.WriteLine($"Sua Cidade: {lista[0]}\nSeu bairro: {lista[1]}\nSua rua: {lista[2]}" );
+                    }
                 }
             }
         }
     }
-    catch(BrasilAPIException exception)
+
+    //Utilização da Dependencia MYSQL para inserir dados do cliente na tabela
+    using (MySqlConnection connection = new MySqlConnection("Server=localhost;Database=apidados;User Id=root;Password=123456;"))
     {
-        Console.WriteLine(exception.Code);
+        connection.Open();
+
+        var valorCidade = listaDadosApi.ElementAt(0);
+        var valorBairro = listaDadosApi.ElementAt(1);
+        var valorRua = listaDadosApi.ElementAt(2);
+        
+        //Variavel que determina os valores que serão enviado aos campos da tabela
+        string valores =
+            "INSERT INTO dadosdouser(firtsname, lastname, idade, cep, cidade, bairro, rua) VALUES ('" +
+            cliente.NomeCliente + "', '" + cliente.UltimoNomeCliente + "', '" + cliente.Idade + "', '" + cliente.Cep+"', '"+valorCidade+"', '"+valorBairro+"', '"+valorRua+"')"; 
+        
+        MySqlCommand command = new MySqlCommand(valores, connection);
+        command.ExecuteNonQuery(); 
+        
+        connection.Close();
+        Console.WriteLine("Connection Closed. Press any key to exit...");
+        Console.ReadKey();
     }
+}
+//Tratamento de Exceções
+catch (SingInError ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (BrasilAPIException exception)
+{
+    Console.WriteLine(exception.Code);
+    Console.WriteLine(exception.Message);
 }
 
